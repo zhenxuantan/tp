e782446ae8a7e55c2f3035a48641647569c746c8
Ambrose Boo <ambroseb9977@gmail.com>
2021-10-04 00:54:58 +0800

    Fix checkstyle errors

--- src/test/java/seedu/address/logic/commands/ClearCommandTest.java
+++ src/test/java/seedu/address/logic/commands/ClearCommandTest.java
@@ -4,7 +4,12 @@ import static seedu.address.logic.commands.CommandTestUtil.assertCommandSuccess;
 import static seedu.address.testutil.TypicalPersons.getTypicalAddressBook;
 
 import org.junit.jupiter.api.Test;
-import seedu.address.model.*;
+
+import seedu.address.model.AddressBook;
+import seedu.address.model.Model;
+import seedu.address.model.ModelManager;
+import seedu.address.model.TaskRecords;
+import seedu.address.model.UserPrefs;
 
 public class ClearCommandTest {
 
