6320cb3284ae1e2211bfef7d3431bf921dd6d60d
JosephN37 <josephnathanael.2014@gmail.com>
2021-10-25 17:41:42 +0800

    add FilterTaskPredicate test

--- src/test/java/seedu/address/model/task/FilterTaskPredicateTest.java
+++ src/test/java/seedu/address/model/task/FilterTaskPredicateTest.java
@@ -5,6 +5,7 @@ import static org.junit.jupiter.api.Assertions.assertTrue;
 import static seedu.address.testutil.Assert.assertThrows;
 
 import org.junit.jupiter.api.Test;
+import seedu.address.commons.core.index.Index;
 
 
 public class FilterTaskPredicateTest {
@@ -30,5 +31,47 @@ public class FilterTaskPredicateTest {
         assertTrue(FilterTaskPredicate.isValidCriterion("d/AaAAa")); // valid, lowercase and uppercase
         assertFalse(FilterTaskPredicate.isValidCriterion("pty/MeD")); // invalid, contains uppercase
         assertTrue(FilterTaskPredicate.isValidCriterion("pty/med")); // valid, lowercase and uppercase
+    }
+
+    @Test
+    public void equals() {
+        final FilterTaskPredicate date = new FilterTaskPredicate("date/2021-11-11");
+        final FilterTaskPredicate pty = new FilterTaskPredicate("pty/high");
+        final FilterTaskPredicate desc = new FilterTaskPredicate("d/swimming");
+        final FilterTaskPredicate type = new FilterTaskPredicate("type/deadline");
+        final FilterTaskPredicate group = new FilterTaskPredicate("g/CS2103T");
+
+        // same object -> returns true
+        assertTrue(date.equals(date));
+        assertTrue(pty.equals(pty));
+        assertTrue(desc.equals(desc));
+        assertTrue(type.equals(type));
+        assertTrue(group.equals(group));
+
+        // same value -> returns true
+        assertTrue(date.equals(new FilterTaskPredicate("date/2021-11-11")));
+        assertTrue(pty.equals(new FilterTaskPredicate("pty/high")));
+        assertTrue(desc.equals(new FilterTaskPredicate("d/swimming")));
+        assertTrue(type.equals(new FilterTaskPredicate("type/deadline")));
+        assertTrue(group.equals(new FilterTaskPredicate("g/CS2103T")));
+
+        // null -> returns false
+        assertFalse(date.equals(null));
+        assertFalse(pty.equals(null));
+        assertFalse(desc.equals(null));
+        assertFalse(type.equals(null));
+        assertFalse(group.equals(null));
+
+        // different types -> returns false
+        assertFalse(date.equals(pty));
+        assertFalse(desc.equals(type));
+        assertFalse(date.equals(group));
+
+        // different value -> returns false
+        assertFalse(date.equals(new FilterTaskPredicate("date/2021-10-11")));
+        assertFalse(pty.equals(new FilterTaskPredicate("pty/low")));
+        assertFalse(desc.equals(new FilterTaskPredicate("d/swim")));
+        assertFalse(type.equals(new FilterTaskPredicate("type/event")));
+        assertFalse(group.equals(new FilterTaskPredicate("g/CS2101")));
     }
 }
