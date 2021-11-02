a83b3a2394f13b1a216c03551e35dfbdc652129a
alinaleehx <alinaleehx@gmail.com>
2021-11-02 21:31:54 +0800

    Fix DoneTaskCommandTest

--- src/test/java/seedu/address/logic/commands/DoneTaskCommandTest.java
+++ src/test/java/seedu/address/logic/commands/DoneTaskCommandTest.java
@@ -7,13 +7,16 @@ import static seedu.address.testutil.TypicalTasks.getTypicalTaskRecords;
 
 import org.junit.jupiter.api.Test;
 
+import javafx.collections.ObservableList;
 import seedu.address.commons.core.Messages;
 import seedu.address.commons.core.index.Index;
 import seedu.address.model.AddressBook;
 import seedu.address.model.Model;
 import seedu.address.model.ModelManager;
+import seedu.address.model.ReadOnlyTaskRecords;
 import seedu.address.model.UserPrefs;
 import seedu.address.model.task.Task;
+import seedu.address.model.task.TaskList;
 
 /**
  * Contains integration tests (interaction with the Model) and unit tests for
@@ -21,25 +24,22 @@ import seedu.address.model.task.Task;
  */
 public class DoneTaskCommandTest {
     //TaskRecords testTaskRecords = new TaskRecords();
-    private Model model = new ModelManager(new AddressBook(), new UserPrefs(), getTypicalTaskRecords());
+    private Task referenceTask = getTypicalTaskRecords().getTaskList().get(INDEX_FIRST_PERSON.getZeroBased());
+    private Task forTest = new Task(referenceTask.getDescription(), referenceTask.getGroup(), referenceTask.getDate(),
+        referenceTask.getTaskType(), referenceTask.getRecurringFrequency(), referenceTask.getPriority());
+    private Model model = new ModelManager(new AddressBook(), new UserPrefs(), new TaskRecordsStub(forTest));
 
     @Test
     public void execute_validIndex_success() {
-        Task doneTask = model.getTasks().get(INDEX_FIRST_PERSON.getZeroBased());
-        Task forTest = new Task(doneTask.getDescription(), doneTask.getGroup(), doneTask.getDate(),
-            doneTask.getTaskType(), doneTask.getRecurringFrequency(), doneTask.getPriority());
         DoneTaskCommand doneTaskCommand = new DoneTaskCommand(INDEX_FIRST_PERSON);
 
-        Task expectedTask = new Task(doneTask.getDescription(), doneTask.getGroup(), doneTask.getDate(),
-            doneTask.getTaskType(), doneTask.getRecurringFrequency(), doneTask.getPriority());
+        Task expectedTask = new Task(referenceTask.getDescription(), referenceTask.getGroup(), referenceTask.getDate(),
+            referenceTask.getTaskType(), referenceTask.getRecurringFrequency(), referenceTask.getPriority());
         expectedTask.markAsDone();
         String expectedMessage = String.format(DoneTaskCommand.MESSAGE_SUCCESS, expectedTask);
-
         ModelManager expectedModel = new ModelManager(model.getAddressBook(), new UserPrefs(), model.getTaskList());
 
         assertCommandSuccess(doneTaskCommand, model, expectedMessage, expectedModel);
-        model.undoDoneTask(doneTask);
-        System.out.println(doneTask.toString());
     }
 
     @Test
@@ -57,6 +57,26 @@ public class DoneTaskCommandTest {
         doneTask.markAsDone();
         assertCommandFailure(doneTaskCommand, model, String.format(DoneTaskCommand.MESSAGE_ALREADY_DONE,
             doneTask.toString()));
+    }
+
+    private class TaskRecordsStub implements ReadOnlyTaskRecords {
+        private final TaskList records = new TaskList();
+        public TaskRecordsStub(Task task) {
+            records.add(task);
+        }
+
+        public void doneTask(Task task) {
+            Task original = task;
+            Task doneTask = task;
+            doneTask.markAsDone();
+            this.records.setTask(original, doneTask);
+        }
+
+        @Override
+        public ObservableList<Task> getTaskList() {
+            return records.asUnmodifiableObservableList();
+        }
+
     }
 
 }
